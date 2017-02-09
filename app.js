20f14c81ad41dd1d548062acb6763773a884b33d
Ahmadou Ndoung <ahmadoum@dstinc.com>
2017-02-09 11:56:09 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-var express = require('express')
-  , app = express()
-  , http = require('http')  
-  , server = http.createServer(app)
-  , io = require('socket.io').listen(server);
-
+var express = require('express');
+var app = express();
+//var http = require('http');
+var http = require('http').Server(app);/
+var io = require('socket.io')(http);
+// var server = http.createServer(app);
+ //var io = require('socket.io').listen(server);
+//Get port
+var port = Number(process.env.PORT || 3000); // if no port is detected, default to 5000
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});
+/*server.listen(port, function() {
+	console.log("Listening on " + port);
+});*/
 /*io.set('transports', [
     'websocket'
   , 'jsonp-polling'
@@ -22,6 +31,8 @@ app.get('/', function (request, response) {
 */
 app.get('/', function (request, response) {
 	response.send("ARI Makelim Push Server");
+	response.send("request: " request);
+	response.send("response: " response);
 });
 
 //app.use(logfmt.requestLogger());
@@ -29,66 +40,96 @@ app.get('/', function (request, response) {
 app.get('/notifyclient/:id', function (request, response){
 	var socket=null;
 	var usersession = request.params.id;
-	if(connections && connections!==null && connections[usersession]){
+	var ok = false;
+	if(connections && connections!==null && connections[usersession] != null){
 		socket = connections[usersession];
+		if (socket.expired) {
+			var message = "Session has expired. (E102)";
+			response.writeHead(404, {"Content-Type": "text/plain"});
+			response.end(message);
+		} else {
 		//for now the message is hard coded as success.
 		socket.volatile.emit('updatecart', usersession, 'Update_Success');
 		response.writeHead(200, {"Content-Type": "text/plain"});
 		response.end("fired updatecart uid:"+usersession+"\n");
+		}
 	}else{
+		var message = "Session not registered. (E100)";
+		response.writeHead(404, {"Content-Type": "text/plain"});
+		response.end(message);
+		/*
 		response.writeHead(404, {"Content-Type": "text/plain"});
 		response.end("Testing by Ahmadou: User not connected. Repeat call.\n");
+		*/
 	}
 });
-
+//List all sessions
+app.get('/listSessions293', function (request, response) {
+	var items = 0;
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	
+	if (connections) {
+		for (var connection in connections) {
+			response.write(connection + "\n");
+			items++;
+		};
+	} 
+	
+	if (items == 0) {
+		response.write("No sessions registered.");
+	} else {
+		response.write("There are " + items + " active sessions.");
+	}
+	
+	response.end();
+});
 // usernames which are currently connected to the chat
 //var usernames = {};
-var port = Number(process.env.PORT || 5000); // if no port is detected, default to 5000
-server.listen(port, function() {
-	console.log("Listening on " + port);
-});
+
+
 // list of connections
 var connections={};
 // 'this' is the socket
 io.on('connection', function (socket) {
-	// when the client emits 'adduser', this listens and executes
-	socket.on('adduser', function(sessionkey){
-		// we store the username in the socket session for this client
-		socket.sessionkey = sessionkey;
-		// add the client's username to the global list
-		connections[sessionkey] = socket;
-		// echo to client they've connected
-		socket.emit('updatechat', 'SERVER', 'you are connected');
-	});
+	
 	socket.on('disconnect', function() {
-		if (this.sessionkey != "undefined") {
-			delete connections[this.sessionkey];
+		if (this.usersession != "undefined") {
+			delete connections[this.usersession];
 		}
-		console.log("**** disconnected session: " + this.sessionkey);
+		console.log("**** disconnected session: " + this.usersession);
 	});
 	
 	socket.on('error', function(err) {
-		console.log("**** session error: " + this.sessionkey + "\n    error: " + err);
+		console.log("**** session error: " + this.usersession + "\n    error: " + err);
 	});
 	
 	socket.on('close', function() {
-		console.log("**** closed socket: " + this.sessionkey);
+		console.log("**** closed socket: " + this.usersession);
 	});
 	
 	socket.on('connection_failed', function() {
-		console.log("**** connection to browser failed for session: " + this.sessionkey);
+		console.log("**** connection to browser failed for session: " + this.usersession);
 	});
 	
 	// listen for client session registration and echo registration success 
-	socket.on('register', function(sessionkey, callback) {
+	socket.on('register', function(usersession, callback) {
 		if (callback != null) {
 			callback('ok');
 		}
-		socket.sessionkey = sessionkey;
+		socket.usersession = usersession;
 		socket.expired = false;
-		connections[sessionkey] = socket;
+		connections[usersession] = socket;
 		
-		console.log("**** registered session: " + sessionkey);
+		console.log("**** registered session: " + usersession);
+		// when the client emits 'adduser', this listens and executes
+	socket.on('adduser', function(usersession){
+		// we store the username in the socket session for this client
+		//socket.usersession = usersession;
+		// add the client's username to the global list
+		//connections[usersession] = socket;
+		// echo to client they've connected
+		socket.emit('updatechat', 'SERVER', 'you are connected');
+	});
 		
 		/* cleanup 
 		for (var connection in connections) {
@@ -97,7 +138,7 @@ io.on('connection', function (socket) {
 	});
 	
 	socket.on('unregister', function() {
-		console.log("**** unregistered session: " + this.sessionkey);
+		console.log("**** unregistered session: " + this.usersession);
 		socket.disconnect();
 	});
 	
@@ -106,11 +147,11 @@ io.on('connection', function (socket) {
 io.sockets.on('connection', function (socket) {
 
 	// when the client emits 'adduser', this listens and executes
-	socket.on('adduser', function(sessionkey){
+	socket.on('adduser', function(usersession){
 		// we store the username in the socket session for this client
-		socket.sessionkey = sessionkey;
+		socket.usersession = usersession;
 		// add the client's username to the global list
-		connections[sessionkey] = socket;
+		connections[usersession] = socket;
 		// echo to client they've connected
 		socket.emit('updatechat', 'SERVER', 'you are connected');
 	});
@@ -118,7 +159,7 @@ io.sockets.on('connection', function (socket) {
 	// when the user disconnects.. perform this
 	socket.on('disconnect', function(){
 		// remove the username from global usernames list
-		delete connections[socket.sessionkey];
+		delete connections[socket.usersession];
 	});
 /*
 	// when the client emits 'sendchat', this listens and executes
